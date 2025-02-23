7b1d7e92dae91e903cb57dbc3e3fe1cb3b1425e5
firoyang <firoyang@tencent.com>
2025-02-23 15:02:08 +0800

    重构一部分代码 新增转换规则

---
+++ VAMVarRenameTool/NameTransform/CreatorPackageNameTransformer.cs
@@ -0,0 +1,26 @@
+﻿using VAMVarRenameTool.MetaData;
+
+namespace VAMVarRenameTool.NameTransform;
+
+public class CreatorPackageNameTransformer : NameTransformer
+{
+    public CreatorPackageNameTransformer (string fileName)
+    {
+        LoadMappings(fileName);
+    }
+    
+    // key是meta里的名字, value是文件名上的名字
+    public bool TryTransform(VarMeta meta)
+    {
+        string inputName = $"{meta.CreatorName}.{meta.PackageName}";
+        if (_nameMap.TryGetValue(inputName, out string output))
+        {
+            var spilit = output.Split('.');
+            meta.CreatorName = spilit[0];
+            meta.PackageName = spilit[1];
+            return true;
+        }
+
+        return false;
+    }
+}
